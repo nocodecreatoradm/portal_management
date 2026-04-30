13d5d26b630a2be2316f2d92c18866e977da69ad
onunez2025 <onunez@sole.com.pegit>
2026-04-30 14:12:53 -0500

    Fix duplicate isUUID in RDInventory.tsx and correct App.tsx syntax

--- src/components/RDInventory.tsx
+++ src/components/RDInventory.tsx
@@ -20,7 +20,6 @@ import { toast } from 'sonner';
 import { SupabaseService } from '../lib/SupabaseService';
 import { initialRDInventory } from '../data/mockData';
 
-const isUUID = (id: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(id);
 
 interface RDInventoryProps {
   initialItems?: RDInventoryItem[];
